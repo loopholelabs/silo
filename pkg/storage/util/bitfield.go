fd19ed96013b37f937864b23a4e8279ee7ffa58d
Jimmy Moore <jamesmoore@loopholelabs.io>
2024-01-16 11:49:59 +0000

    Added bitfield to track chunks status etc

---
+++ pkg/storage/util/bitfield.go
@@ -0,0 +1,180 @@
+package util
+
+import "sync/atomic"
+
+type Bitfield struct {
+	data []uint64
+}
+
+func NewBitfield(size int) *Bitfield {
+	return &Bitfield{
+		data: make([]uint64, (size+63)>>6),
+	}
+}
+
+/**
+ * Set the specified bit
+ *
+ */
+func (bf *Bitfield) SetBit(i int) {
+	f := uint64(1 << (i & 63))
+	p := i >> 6
+	old := atomic.LoadUint64(&bf.data[p])
+	for !atomic.CompareAndSwapUint64(&bf.data[p], old, old|f) {
+		old = atomic.LoadUint64(&bf.data[p])
+	}
+}
+
+/**
+ * Clear the specified bit
+ *
+ */
+func (bf *Bitfield) ClearBit(i int) {
+	f := uint64(1 << (i & 63))
+	p := i >> 6
+	old := atomic.LoadUint64(&bf.data[p])
+	for !atomic.CompareAndSwapUint64(&bf.data[p], old, old&^f) {
+		old = atomic.LoadUint64(&bf.data[p])
+	}
+}
+
+/**
+ * Check if the specified bit is currently set
+ *
+ */
+func (bf *Bitfield) BitSet(i int) bool {
+	f := uint64(1 << (i & 63))
+	p := i >> 6
+	old := atomic.LoadUint64(&bf.data[p])
+	return (old & f) != 0
+}
+
+func (bf *Bitfield) Length() uint {
+	return uint(len(bf.data) >> 6)
+}
+
+// Create a mask of bits at the start of this range
+func maskStart(start, end uint) (mask uint64) {
+	const max = ^uint64(0)
+	return ((max << (start & 63)) ^ (max << (end - start&^63))) & ((1 >> (start & 63)) - 1)
+}
+
+// Create a mask of bits at the end of this range
+func maskEnd(start, end uint) (mask uint64) {
+	const shiftBy = 31 + 32*(^uint(0)>>63)
+	return ((1 << (end & 63)) - 1) & uint64((((end&^63-start)>>shiftBy)&1)-1)
+}
+
+/**
+ * Set a number of bits in one op
+ *
+ */
+func (bf *Bitfield) SetBits(start uint, end uint) {
+	/*
+		if start > end {
+			panic("End less than start")
+		}
+		if end > bf.Length() {
+			panic("Out of range")
+		}
+	*/
+
+	if mask := maskStart(start, end); mask != 0 {
+		p := start >> 6
+		old := atomic.LoadUint64(&bf.data[p])
+		for !atomic.CompareAndSwapUint64(&bf.data[p], old, old|mask) {
+			old = atomic.LoadUint64(&bf.data[p])
+		}
+	}
+
+	// Fill in any middle section
+	for i := (start + 63) &^ 63; i < end&^63; i += 64 {
+		p := i >> 6
+		atomic.StoreUint64(&bf.data[p], ^uint64(0))
+	}
+
+	if mask := maskEnd(start, end); mask != 0 {
+		p := end >> 6
+		old := atomic.LoadUint64(&bf.data[p])
+		for !atomic.CompareAndSwapUint64(&bf.data[p], old, old|mask) {
+			old = atomic.LoadUint64(&bf.data[p])
+		}
+	}
+}
+
+/**
+ * Clear a number of bits in one op
+ *
+ */
+func (bf *Bitfield) ClearBits(start uint, end uint) {
+	/*
+		if start > end {
+			panic("End less than start")
+		}
+		if end > bf.Length() {
+			panic("Out of range")
+		}
+	*/
+	if mask := maskStart(start, end); mask != 0 {
+		p := start >> 6
+		old := atomic.LoadUint64(&bf.data[p])
+		for !atomic.CompareAndSwapUint64(&bf.data[p], old, old&^mask) {
+			old = atomic.LoadUint64(&bf.data[p])
+		}
+	}
+
+	// Fill in any middle section
+	for i := (start + 63) &^ 63; i < end&^63; i += 64 {
+		p := i >> 6
+		atomic.StoreUint64(&bf.data[p], 0)
+	}
+
+	if mask := maskEnd(start, end); mask != 0 {
+		p := end >> 6
+		old := atomic.LoadUint64(&bf.data[p])
+		for !atomic.CompareAndSwapUint64(&bf.data[p], old, old&^mask) {
+			old = atomic.LoadUint64(&bf.data[p])
+		}
+	}
+
+}
+
+/**
+ * Check if a range of bits are all set
+ * NB This is NOT atomic with SetBits/ClearBits. So there may be a set/clear that is partially completed at this check.
+ */
+func (bf *Bitfield) BitsSet(start uint, end uint) bool {
+	/*
+		if start > end {
+			panic("End less than start")
+		}
+		if end > bf.Length() {
+			panic("Out of range")
+		}
+	*/
+
+	p := start >> 6
+	i := uint64(1 << (start & 63))
+	n := start
+	val := atomic.LoadUint64(&bf.data[p])
+	for {
+		if n == end {
+			break
+		}
+		// Check the bit
+		if (val & i) == 0 {
+			return false
+		}
+
+		// Move along one...
+		n++
+		i = i << 1
+		if i == 0 {
+			i = 1
+			p++
+			val = atomic.LoadUint64(&bf.data[p])
+		}
+	}
+
+	return true
+}
---
+++ pkg/storage/util/bitfield_test.go
@@ -0,0 +1,92 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBitfieldSetBit(t *testing.T) {
+	bf := NewBitfield(1000)
+
+	bf.SetBit(99)
+
+	assert.Equal(t, true, bf.BitSet(99))
+	assert.Equal(t, false, bf.BitSet(100))
+
+	bf.SetBit(99) // Check it doesn't do anything
+	bf.SetBit(100)
+
+	assert.Equal(t, true, bf.BitSet(99))
+	assert.Equal(t, true, bf.BitSet(100))
+}
+
+func TestBitfieldClearBit(t *testing.T) {
+	bf := NewBitfield(1000)
+
+	bf.SetBit(99)
+
+	assert.Equal(t, true, bf.BitSet(99))
+	assert.Equal(t, false, bf.BitSet(100))
+
+	bf.ClearBit(99)
+	bf.ClearBit(100) // Check it doesn't do anything
+
+	assert.Equal(t, false, bf.BitSet(99))
+	assert.Equal(t, false, bf.BitSet(100))
+}
+
+func TestBitfieldSetBits(t *testing.T) {
+	bf := NewBitfield(1000)
+	// TODO: Add a few test cases here...
+	bf.SetBits(100, 180)
+
+	for i := 0; i < 100; i++ {
+		assert.Equal(t, false, bf.BitSet(i))
+	}
+
+	for i := 100; i < 180; i++ {
+		assert.Equal(t, true, bf.BitSet(i))
+	}
+
+	for i := 180; i < 1000; i++ {
+		assert.Equal(t, false, bf.BitSet(i))
+	}
+
+}
+
+func TestBitfieldClearBits(t *testing.T) {
+	bf := NewBitfield(1000)
+	bf.SetBits(0, 1000)
+
+	// TODO: Add a few test cases here...
+	bf.ClearBits(100, 180)
+
+	for i := 0; i < 100; i++ {
+		assert.Equal(t, true, bf.BitSet(i))
+	}
+
+	for i := 100; i < 180; i++ {
+		assert.Equal(t, false, bf.BitSet(i))
+	}
+
+	for i := 180; i < 1000; i++ {
+		assert.Equal(t, true, bf.BitSet(i))
+	}
+
+}
+
+func TestBitfieldBitsSet(t *testing.T) {
+	bf := NewBitfield(1000)
+	// TODO: Add a few test cases here...
+	bf.SetBits(100, 180)
+
+	assert.Equal(t, true, bf.BitsSet(100, 180))
+	assert.Equal(t, false, bf.BitsSet(100, 185))
+	assert.Equal(t, false, bf.BitsSet(0, 1000))
+
+	// Make a gap
+	bf.ClearBit(150)
+	assert.Equal(t, false, bf.BitsSet(100, 180))
+
+}
