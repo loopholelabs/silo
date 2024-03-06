1742bce0bf03bf2bd9c6eb7e1845ceba0b8003c0
Jimmy Moore <jamesmoore@loopholelabs.io>
2024-03-06 16:24:39 +0000

    Added lock on dirtyTracker

--- pkg/storage/dirtytracker/read_dirty_tracker.go
+++ pkg/storage/dirtytracker/read_dirty_tracker.go
@@ -1,13 +1,15 @@
 package dirtytracker
 
 import (
+	"sync"
+
 	"github.com/loopholelabs/silo/pkg/storage"
 	"github.com/loopholelabs/silo/pkg/storage/util"
 )
 
 /**
  * This can track writes so that we can get a list of dirty areas after some period.
- * Tracking is enabled for a block when a Read is performed on that block.
+ * Tracking is enabled for a block when a Read is performed on the remote for that block.
  * A call to sync() grabs the list of dirty blocks, and resets those to not be monitored.
  */
 
@@ -18,6 +20,7 @@ type DirtyTracker struct {
 	numBlocks int
 	dirtyLog  *util.Bitfield
 	tracking  *util.Bitfield
+	writeLock sync.Mutex
 }
 
 type DirtyTrackerLocal struct {
@@ -75,11 +78,15 @@ func NewDirtyTracker(prov storage.StorageProvider, blockSize int) (*DirtyTracker
 }
 
 func (i *DirtyTrackerRemote) Sync() *util.Bitfield {
+	i.dt.writeLock.Lock()
+	defer i.dt.writeLock.Unlock()
+
 	info := i.dt.dirtyLog.Clone()
 
-	// Remove these blocks from tracking... (They will get added again when a Read is performed to migrate the data)
+	// Remove the dirty blocks from tracking... (They will get added again when a Read is performed to migrate the data)
 	i.dt.tracking.ClearBitsIf(info, 0, uint(i.dt.numBlocks))
 
+	// Clear the dirty log.
 	i.dt.dirtyLog.Clear()
 	return info
 }
@@ -89,17 +96,23 @@ func (i *DirtyTracker) localReadAt(buffer []byte, offset int64) (int, error) {
 }
 
 func (i *DirtyTracker) localWriteAt(buffer []byte, offset int64) (int, error) {
-	end := uint64(offset + int64(len(buffer)))
-	if end > i.size {
-		end = i.size
+	i.writeLock.Lock()
+	defer i.writeLock.Unlock()
+
+	n, err := i.prov.WriteAt(buffer, offset)
+
+	if err == nil {
+		end := uint64(offset + int64(len(buffer)))
+		if end > i.size {
+			end = i.size
+		}
+
+		b_start := uint(offset / int64(i.blockSize))
+		b_end := uint((end-1)/uint64(i.blockSize)) + 1
+
+		i.dirtyLog.SetBitsIf(i.tracking, b_start, b_end)
 	}
-
-	b_start := uint(offset / int64(i.blockSize))
-	b_end := uint((end-1)/uint64(i.blockSize)) + 1
-
-	i.dirtyLog.SetBitsIf(i.tracking, b_start, b_end)
-
-	return i.prov.WriteAt(buffer, offset)
+	return n, err
 }
 
 func (i *DirtyTracker) localFlush() error {
@@ -119,10 +132,15 @@ func (i *DirtyTracker) remoteReadAt(buffer []byte, offset int64) (int, error) {
 	b_start := uint(offset / int64(i.blockSize))
 	b_end := uint((end-1)/uint64(i.blockSize)) + 1
 
+	// FIXME: At the moment we lock the entire region. We could do better with range locking.
+	i.writeLock.Lock()
+
 	// Enable tracking for this area
 	i.tracking.SetBits(b_start, b_end)
-	// FIXME: If a write happens here, then we'll get a dirtyLog for it where we don't need one. We should couple the SetBits to the ReadAt.
-	return i.prov.ReadAt(buffer, offset)
+	n, err := i.prov.ReadAt(buffer, offset)
+
+	i.writeLock.Unlock()
+	return n, err
 }
 
 func (i *DirtyTracker) remoteWriteAt(buffer []byte, offset int64) (int, error) {
