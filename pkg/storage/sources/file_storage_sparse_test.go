74a9da459e57a2d4b46ca1966079d2a74d5e3dbd
Jimmy Moore <jamesmoore@loopholelabs.io>
2024-03-27 17:23:14 +0000

    FileStorageSparse can now open existing

--- pkg/storage/sources/file_storage_sparse_test.go
+++ pkg/storage/sources/file_storage_sparse_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFileStorageSparse(t *testing.T) {
+func TestFileStorageSparseCreate(t *testing.T) {
 
 	source, err := NewFileStorageSparseCreate("test_data_sparse", 100, 10)
 	assert.NoError(t, err)
@@ -30,4 +30,41 @@ func TestFileStorageSparse(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, data, buffer)
+}
+
+func TestFileStorageSparse(t *testing.T) {
+
+	source, err := NewFileStorageSparseCreate("test_data_sparse", 100, 10)
+	assert.NoError(t, err)
+
+	t.Cleanup(func() {
+		os.Remove("test_data_sparse")
+		os.Remove("test_data_sparse.offsets")
+	})
+
+	data := make([]byte, 30)
+	rand.Read(data)
+
+	_, err = source.WriteAt(data, 0)
+	assert.NoError(t, err)
+
+	// Try reading it back...
+
+	buffer := make([]byte, len(data))
+	_, err = source.ReadAt(buffer, 0)
+	assert.NoError(t, err)
+
+	assert.Equal(t, data, buffer)
+
+	source.Flush() // This will write the offsets file
+
+	source2, err := NewFileStorageSparse("test_data_sparse", 100, 10)
+	assert.NoError(t, err)
+
+	buffer2 := make([]byte, len(data))
+	_, err = source2.ReadAt(buffer2, 0)
+	assert.NoError(t, err)
+
+	assert.Equal(t, data, buffer2)
+
 }
