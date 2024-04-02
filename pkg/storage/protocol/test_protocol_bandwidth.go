722141191034f6e53b46a43ac23bc3455e2af62f
Jimmy Moore <jamesmoore@loopholelabs.io>
2024-04-02 15:52:48 +0100

    Added bandwidth/latency protocol tests

---
+++ pkg/storage/protocol/test_protocol_bandwidth.go
@@ -0,0 +1,107 @@
+package protocol
+
+import (
+	"io"
+	"sync"
+	"time"
+)
+
+type packetInfo struct {
+	ctime time.Time
+	bytes uint64
+}
+
+type TestProtocolBandwidth struct {
+	proto             Protocol
+	recvBandwidth     uint64 // In bytes per second
+	recentPacketsLock sync.Mutex
+	recentPackets     []*packetInfo
+}
+
+func NewTestProtocolBandwidth(proto Protocol, recvBandwidth uint64) Protocol {
+	p := &TestProtocolBandwidth{
+		proto:         proto,
+		recvBandwidth: recvBandwidth,
+		recentPackets: make([]*packetInfo, 0),
+	}
+
+	return p
+}
+
+func (p *TestProtocolBandwidth) addRecentPacket(l uint64) {
+	p.recentPacketsLock.Lock()
+	defer p.recentPacketsLock.Unlock()
+	p.recentPackets = append(p.recentPackets, &packetInfo{
+		ctime: time.Now(),
+		bytes: l,
+	})
+}
+
+func (p *TestProtocolBandwidth) checkBandwidth(since time.Duration) (uint64, time.Duration) {
+	p.recentPacketsLock.Lock()
+	defer p.recentPacketsLock.Unlock()
+	totalBytes := uint64(0)
+	earliestTime := time.Now()
+	for _, pi := range p.recentPackets {
+		if time.Since(pi.ctime) < since {
+			totalBytes += pi.bytes
+			if pi.ctime.Before(earliestTime) {
+				earliestTime = pi.ctime
+			}
+		}
+	}
+	return totalBytes, time.Since(earliestTime)
+}
+
+func (p *TestProtocolBandwidth) expireBandwidth(since time.Duration) {
+	p.recentPacketsLock.Lock()
+	defer p.recentPacketsLock.Unlock()
+	newRecentPackets := make([]*packetInfo, 0)
+	for _, pi := range p.recentPackets {
+		if time.Since(pi.ctime) < since {
+			newRecentPackets = append(newRecentPackets, pi)
+		}
+	}
+	p.recentPackets = newRecentPackets
+}
+
+func (p *TestProtocolBandwidth) SendPacketWriter(dev uint32, id uint32, length uint32, data func(w io.Writer) error) (uint32, error) {
+	return p.proto.SendPacketWriter(dev, id, length, data)
+}
+
+func (p *TestProtocolBandwidth) SendPacket(dev uint32, id uint32, data []byte) (uint32, error) {
+	return p.proto.SendPacket(dev, id, data)
+}
+
+func (p *TestProtocolBandwidth) waitForBandwidth() {
+	for {
+		bytes, duration := p.checkBandwidth(1 * time.Second)
+		if duration.Nanoseconds() > 0 {
+			// duration may be less than a second, in which case we should adjust bytes...
+			sf := float64(time.Second.Nanoseconds()) / float64(duration.Nanoseconds())
+			adj_bytes := uint64(float64(bytes) * sf)
+			if adj_bytes < p.recvBandwidth {
+				p.expireBandwidth(1 * time.Second)
+				break
+			}
+		} else {
+			break
+		}
+		// Wait a little to free some bandwidth...
+		time.Sleep(5 * time.Millisecond)
+	}
+}
+
+func (p *TestProtocolBandwidth) WaitForPacket(dev uint32, id uint32) ([]byte, error) {
+	p.waitForBandwidth()
+	data, err := p.proto.WaitForPacket(dev, id)
+	p.addRecentPacket(uint64(len(data) + 12))
+	return data, err
+}
+
+func (p *TestProtocolBandwidth) WaitForCommand(dev uint32, cmd byte) (uint32, []byte, error) {
+	p.waitForBandwidth()
+	id, data, err := p.proto.WaitForCommand(dev, cmd)
+	p.addRecentPacket(uint64(len(data) + 12))
+	return id, data, err
+}
---
+++ pkg/storage/protocol/test_protocol_latency_test.go
@@ -0,0 +1,52 @@
+package protocol
+
+import (
+	"context"
+	"crypto/rand"
+	"testing"
+	"time"
+
+	"github.com/loopholelabs/silo/pkg/storage"
+	"github.com/loopholelabs/silo/pkg/storage/sources"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTestProtocolLatency(t *testing.T) {
+
+	size := 1024 * 1024
+	var store storage.StorageProvider
+
+	// Setup a protocol in the middle, and make sure our reads/writes get through ok
+
+	prm := NewMockProtocol()
+	// Add some recv latency
+	pr := NewTestProtocolLatency(prm, 50*time.Millisecond)
+
+	sourceToProtocol := NewToProtocol(uint64(size), 1, pr)
+
+	storeFactory := func(di *DevInfo) storage.StorageProvider {
+		store = sources.NewMemoryStorage(int(di.Size))
+		return store
+	}
+
+	destFromProtocol := NewFromProtocol(1, storeFactory, pr)
+
+	go destFromProtocol.HandleDevInfo()
+	go destFromProtocol.HandleSend(context.TODO())
+	go destFromProtocol.HandleReadAt()
+	go destFromProtocol.HandleWriteAt()
+
+	// Send devInfo
+	sourceToProtocol.SendDevInfo("test", 4096)
+
+	buff := make([]byte, 4096)
+	rand.Read(buff)
+	ctime := time.Now()
+	n, err := sourceToProtocol.WriteAt(buff, 12)
+
+	assert.NoError(t, err)
+	assert.Equal(t, len(buff), n)
+
+	// Check it took some time (50ms for WriteAt, and 50ms for WriteAtResponse)
+	assert.WithinDuration(t, ctime.Add(100*time.Millisecond), time.Now(), 10*time.Millisecond)
+}
