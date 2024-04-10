e116f593dff9ab5d7d569fd60ee9a488ad6992b7
Jimmy Moore <jamesmoore@loopholelabs.io>
2024-04-10 16:39:38 +0100

    Data race fix on test latency

--- pkg/storage/protocol/test_protocol_latency.go
+++ pkg/storage/protocol/test_protocol_latency.go
@@ -2,6 +2,7 @@ package protocol
 
 import (
 	"io"
+	"sync/atomic"
 	"time"
 )
 
@@ -13,16 +14,16 @@ import (
 type TestProtocolLatency struct {
 	proto       Protocol
 	recvLatency time.Duration
-	isFirst     bool
+	isFirst     atomic.Bool
 }
 
 func NewTestProtocolLatency(proto Protocol, recvLatency time.Duration) Protocol {
 	p := &TestProtocolLatency{
 		proto:       proto,
 		recvLatency: recvLatency,
-		isFirst:     true,
 	}
 
+	p.isFirst.Store(true)
 	return p
 }
 
@@ -35,17 +36,17 @@ func (p *TestProtocolLatency) SendPacket(dev uint32, id uint32, data []byte) (ui
 }
 
 func (p *TestProtocolLatency) WaitForPacket(dev uint32, id uint32) ([]byte, error) {
-	if p.isFirst {
+	if p.isFirst.Load() {
 		time.Sleep(p.recvLatency)
-		p.isFirst = false
+		p.isFirst.Store(false)
 	}
 	return p.proto.WaitForPacket(dev, id)
 }
 
 func (p *TestProtocolLatency) WaitForCommand(dev uint32, cmd byte) (uint32, []byte, error) {
-	if p.isFirst {
+	if p.isFirst.Load() {
 		time.Sleep(p.recvLatency)
-		p.isFirst = false
+		p.isFirst.Store(false)
 	}
 	return p.proto.WaitForCommand(dev, cmd)
 }
