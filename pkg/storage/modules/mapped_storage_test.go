1c095b034e8afcd00efc7e766f60e4bb6b97b9fc
Jimmy Moore <jamesmoore@loopholelabs.io>
2024-05-16 15:12:16 +0100

    Added couple of tests for MappedStorage addresses/regions

--- pkg/storage/modules/mapped_storage_test.go
+++ pkg/storage/modules/mapped_storage_test.go
@@ -2,6 +2,7 @@ package modules
 
 import (
 	"crypto/rand"
+	"slices"
 	"testing"
 
 	"github.com/loopholelabs/silo/pkg/storage/sources"
@@ -90,5 +91,61 @@ func TestMappedStorageDupe(t *testing.T) {
 	assert.ErrorIs(t, err, Err_not_found)
 
 	assert.Equal(t, uint64(block_size), ms.Size())
+
+}
+
+func TestMappedStorageGetAddresses(t *testing.T) {
+	block_size := 4096
+	store := sources.NewMemoryStorage(64 * block_size)
+
+	ms := NewMappedStorage(store, block_size)
+	// Write some blocks, then read them back
+
+	data := make([]byte, block_size)
+	_, err := rand.Read(data)
+	assert.NoError(t, err)
+
+	err = ms.WriteBlock(0x12345678, data)
+	assert.NoError(t, err)
+
+	err = ms.WriteBlock(0x12345678+uint64(block_size), data)
+	assert.NoError(t, err)
+
+	addresses := ms.GetBlockAddresses()
+	slices.Sort(addresses)
+
+	assert.Equal(t, []uint64{0x12345678, 0x12346678}, addresses)
+}
+
+func TestMappedStorageGetRegions(t *testing.T) {
+	block_size := 4096
+	store := sources.NewMemoryStorage(64 * block_size)
+
+	ms := NewMappedStorage(store, block_size)
+	// Write some blocks, then read them back
+
+	data := make([]byte, block_size)
+	_, err := rand.Read(data)
+	assert.NoError(t, err)
+
+	// Make a region here
+	for i := 0; i < 8; i++ {
+		err = ms.WriteBlock(0x12345678+uint64(i*block_size), data)
+		assert.NoError(t, err)
+
+	}
+
+	// Single block here
+	err = ms.WriteBlock(0x20000000, data)
+	assert.NoError(t, err)
+
+	addresses := ms.GetBlockAddresses()
+	regions := ms.GetRegions(addresses, uint64(block_size*6))
+
+	assert.Equal(t, map[uint64]uint64{
+		305419896: 24576,
+		305444472: 8192,
+		536870912: 4096,
+	}, regions)
 
 }
