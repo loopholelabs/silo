91a232c98ad4450121a67a1675d8ff6f175d6699
Jimmy Moore <jamesmoore@loopholelabs.io>
2024-12-09 17:28:02 +0000

    First test for deviceGroup
    Signed-off-by: Jimmy Moore <jamesmoore@loopholelabs.io>

--- pkg/storage/devicegroup/device_group_test.go
+++ pkg/storage/devicegroup/device_group_test.go
@@ -1,1 +1,38 @@
 package devicegroup
+
+import (
+	"fmt"
+	"os/user"
+	"testing"
+
+	"github.com/loopholelabs/silo/pkg/storage/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDeviceGroupBasic(t *testing.T) {
+	currentUser, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	if currentUser.Username != "root" {
+		fmt.Printf("Cannot run test unless we are root.\n")
+		return
+	}
+
+	ds := []*config.DeviceSchema{
+		{
+			Name:      "test1",
+			Size:      "8m",
+			System:    "file",
+			BlockSize: "1m",
+			Expose:    true,
+			Location:  "testdev_test1",
+		},
+	}
+
+	dg, err := New(ds, nil, nil)
+	assert.NoError(t, err)
+
+	err = dg.CloseAll()
+	assert.NoError(t, err)
+}
