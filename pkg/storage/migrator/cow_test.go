f6c568cd8250da62cf8456cd860526e40bc9a1de
Jimmy Moore <jamesmoore@loopholelabs.io>
2025-01-31 20:13:07 +0000

    Added test for cow migration
    Signed-off-by: Jimmy Moore <jamesmoore@loopholelabs.io>

---
+++ pkg/storage/migrator/cow_test.go
@@ -0,0 +1,111 @@
+package migrator
+
+import (
+	"context"
+	crand "crypto/rand"
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/loopholelabs/silo/pkg/storage"
+	"github.com/loopholelabs/silo/pkg/storage/blocks"
+	"github.com/loopholelabs/silo/pkg/storage/dirtytracker"
+	"github.com/loopholelabs/silo/pkg/storage/modules"
+	"github.com/loopholelabs/silo/pkg/storage/protocol"
+	"github.com/loopholelabs/silo/pkg/storage/protocol/packets"
+	"github.com/loopholelabs/silo/pkg/storage/sources"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMigratorCow(t *testing.T) {
+	size := 10 * 1024 * 1024
+	blockSize := 4096
+	numBlocks := (size + blockSize - 1) / blockSize
+
+	sourceStorageMem := sources.NewMemoryStorage(size)
+	sourceDirtyLocal, sourceDirtyRemote := dirtytracker.NewDirtyTracker(sourceStorageMem, blockSize)
+	sourceStorage := modules.NewLockable(sourceDirtyLocal)
+
+	// Set up some data here.
+	buffer := make([]byte, size)
+	_, err := crand.Read(buffer)
+	assert.NoError(t, err)
+
+	n, err := sourceStorage.WriteAt(buffer, 0)
+	assert.NoError(t, err)
+	assert.Equal(t, len(buffer), n)
+
+	orderer := blocks.NewAnyBlockOrder(numBlocks, nil)
+	orderer.AddAll()
+
+	// START moving data from sourceStorage to destStorage
+
+	var destStorage storage.Provider
+	var destFrom *protocol.FromProtocol
+
+	// Create a simple pipe
+	r1, w1 := io.Pipe()
+	r2, w2 := io.Pipe()
+
+	initDev := func(ctx context.Context, p protocol.Protocol, dev uint32) {
+		destStorageFactory := func(di *packets.DevInfo) storage.Provider {
+			destStorage = sources.NewMemoryStorage(int(di.Size))
+			return destStorage
+		}
+
+		// Pipe from the protocol to destWaiting
+		destFrom = protocol.NewFromProtocol(ctx, dev, destStorageFactory, p)
+		go func() {
+			_ = destFrom.HandleReadAt()
+		}()
+		go func() {
+			_ = destFrom.HandleWriteAt()
+		}()
+		go func() {
+			_ = destFrom.HandleDevInfo()
+		}()
+	}
+
+	prSource := protocol.NewRW(context.TODO(), []io.Reader{r1}, []io.Writer{w2}, nil)
+	prDest := protocol.NewRW(context.TODO(), []io.Reader{r2}, []io.Writer{w1}, initDev)
+
+	go func() {
+		_ = prSource.Handle()
+	}()
+	go func() {
+		_ = prDest.Handle()
+	}()
+
+	// Pipe a destination to the protocol
+	destination := protocol.NewToProtocol(sourceDirtyRemote.Size(), 17, prSource)
+
+	err = destination.SendDevInfo("test", uint32(blockSize), "")
+	assert.NoError(t, err)
+
+	conf := NewConfig().WithBlockSize(blockSize)
+	conf.LockerHandler = sourceStorage.Lock
+	conf.UnlockerHandler = sourceStorage.Unlock
+
+	mig, err := NewMigrator(sourceDirtyRemote,
+		destination,
+		orderer,
+		conf)
+
+	assert.NoError(t, err)
+
+	err = mig.Migrate(numBlocks)
+	assert.NoError(t, err)
+
+	err = mig.WaitForCompletion()
+	assert.NoError(t, err)
+
+	// This will end with migration completed, and consumer Locked.
+	eq, err := storage.Equals(sourceStorageMem, destStorage, blockSize)
+	assert.NoError(t, err)
+	assert.True(t, eq)
+
+	srcDataSent := prSource.GetMetrics().DataSent
+	srcDataRecv := prSource.GetMetrics().DataRecv
+
+	fmt.Printf("Transfer [device size %d] transfer bytes: %d sent, %d recv\n", size, srcDataSent, srcDataRecv)
+}
