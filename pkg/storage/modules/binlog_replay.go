341ac0b858401905c69553f010975ff395c90731
Jimmy Moore <67790371+jimmyaxod@users.noreply.github.com>
2025-02-07 18:39:43 +0000

    Improved binlog replay. Can now use gz, skip executions, and better e… (#82)
    * Improved binlog replay. Can now use gz, skip executions, and better err returns
    
    Signed-off-by: Jimmy Moore <jamesmoore@loopholelabs.io>
    
    * Fix build for binlog replay changes
    
    Signed-off-by: Jimmy Moore <jamesmoore@loopholelabs.io>
    
    ---------
    
    Signed-off-by: Jimmy Moore <jamesmoore@loopholelabs.io>

--- pkg/storage/modules/binlog_replay.go
+++ pkg/storage/modules/binlog_replay.go
@@ -1,8 +1,12 @@
 package modules
 
 import (
+	"compress/gzip"
 	"encoding/binary"
+	"errors"
+	"io"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/loopholelabs/silo/pkg/storage"
@@ -10,40 +14,73 @@ import (
 )
 
 type BinLogReplay struct {
-	prov     storage.Provider
-	fp       *os.File
-	ctime    time.Time
-	setCtime bool
+	prov       storage.Provider
+	fp         *os.File
+	r          io.Reader
+	gzipReader *gzip.Reader
+	ctime      time.Time
+	setCtime   bool
 }
 
 func NewBinLogReplay(filename string, prov storage.Provider) (*BinLogReplay, error) {
+	var r io.Reader
+	var gzipReader *gzip.Reader
 	fp, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
+
+	r = fp
+	// If the filename ends with .gz, use gzip to decompress as we go.
+	if strings.HasSuffix(filename, ".gz") {
+		gzipReader, err = gzip.NewReader(fp)
+		if err != nil {
+			return nil, err
+		}
+		r = gzipReader
+	}
+
 	return &BinLogReplay{
-		fp:       fp,
-		ctime:    time.Now(),
-		setCtime: true,
-		prov:     prov,
+		fp:         fp,
+		gzipReader: gzipReader,
+		r:          r,
+		ctime:      time.Now(),
+		setCtime:   true,
+		prov:       prov,
 	}, nil
+}
+
+func (i *BinLogReplay) Close() error {
+	var err error
+	if i.gzipReader != nil {
+		err = i.gzipReader.Close()
+	}
+	return errors.Join(err, i.fp.Close())
 }
 
 /**
  * Execute the next read/write.
  * If speed is 0 then it's executed immediately, else we wait.
  */
-func (i *BinLogReplay) ExecuteNext(speed float64) error {
+func (i *BinLogReplay) Next(speed float64, execute bool) (error, error) {
 	// Read a header
 	header := make([]byte, 12)
-	_, err := i.fp.Read(header)
+	_, err := io.ReadFull(i.r, header)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	if i.setCtime {
 		i.ctime = time.Now()
 		i.setCtime = false
+	}
+
+	length := binary.LittleEndian.Uint32(header[8:])
+
+	data := make([]byte, length)
+	_, err = io.ReadFull(i.r, data)
+	if err != nil {
+		return nil, err
 	}
 
 	// If we need to, we'll wait here until the next log should be replayed.
@@ -54,30 +91,25 @@ func (i *BinLogReplay) ExecuteNext(speed float64) error {
 		time.Sleep(time.Duration(delay))
 	}
 
-	length := binary.LittleEndian.Uint32(header[8:])
-	data := make([]byte, length)
-	_, err = i.fp.Read(data)
-	if err != nil {
-		return err
-	}
-
-	// Dispatch the command
-	if data[0] == packets.CommandReadAt {
-		offset, length, err := packets.DecodeReadAt(data)
-		if err != nil {
-			return err
+	if execute {
+		// Dispatch the command
+		if data[0] == packets.CommandReadAt {
+			offset, length, err := packets.DecodeReadAt(data)
+			if err != nil {
+				return nil, err
+			}
+			buffer := make([]byte, length)
+			_, err = i.prov.ReadAt(buffer, offset)
+			return err, nil
+		} else if data[0] == packets.CommandWriteAt {
+			offset, buffer, err := packets.DecodeWriteAt(data)
+			if err != nil {
+				return nil, err
+			}
+			_, err = i.prov.WriteAt(buffer, offset)
+			return err, nil
 		}
-		buffer := make([]byte, length)
-		_, err = i.prov.ReadAt(buffer, offset)
-		return err
-	} else if data[0] == packets.CommandWriteAt {
-		offset, buffer, err := packets.DecodeWriteAt(data)
-		if err != nil {
-			return err
-		}
-		_, err = i.prov.WriteAt(buffer, offset)
-		return err
+		return nil, errors.New("unknown packet in binlog")
 	}
-	panic("Unknown packet in binlog")
-
+	return nil, nil
 }
