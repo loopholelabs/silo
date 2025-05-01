77fa2f08b8bf740170c6b74d33ee0715f99c2d0f
Jimmy Moore <jamesmoore@loopholelabs.io>
2025-05-01 16:21:22 +0100

    Added storage_test to check equals and difference utils
    Signed-off-by: Jimmy Moore <jamesmoore@loopholelabs.io>

---
+++ pkg/storage/storage_test.go
@@ -0,0 +1,74 @@
+package storage_test
+
+import (
+	"crypto/rand"
+	"testing"
+
+	"github.com/loopholelabs/silo/pkg/storage"
+	"github.com/loopholelabs/silo/pkg/storage/sources"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStorageDifference(t *testing.T) {
+	sp1 := sources.NewMemoryStorage(1024 * 1024)
+	sp2 := sources.NewMemoryStorage(1024 * 1024)
+
+	d := make([]byte, sp1.Size())
+	_, err := rand.Read(d)
+	assert.NoError(t, err)
+	n, err := sp1.WriteAt(d, 0)
+	assert.NoError(t, err)
+	assert.Equal(t, len(d), n)
+	n, err = sp2.WriteAt(d, 0)
+	assert.NoError(t, err)
+	assert.Equal(t, len(d), n)
+
+	// Now modify sp2 a bit
+	for _, offset := range []int64{7, 8, 900, 3000} {
+		d = make([]byte, 6)
+		_, err = rand.Read(d)
+		assert.NoError(t, err)
+		n, err = sp2.WriteAt(d, offset)
+		assert.NoError(t, err)
+		assert.Equal(t, len(d), n)
+	}
+
+	diffBlocks, diffBytes, err := storage.Difference(sp1, sp2, 1024)
+	assert.NoError(t, err)
+	assert.Equal(t, uint64(2), diffBlocks)
+	assert.Equal(t, uint64(19), diffBytes)
+}
+
+func TestEquals(t *testing.T) {
+	sp1 := sources.NewMemoryStorage(1024 * 1024)
+	sp2 := sources.NewMemoryStorage(1024 * 1024)
+
+	d := make([]byte, sp1.Size())
+	_, err := rand.Read(d)
+	assert.NoError(t, err)
+	n, err := sp1.WriteAt(d, 0)
+	assert.NoError(t, err)
+	assert.Equal(t, len(d), n)
+	n, err = sp2.WriteAt(d, 0)
+	assert.NoError(t, err)
+	assert.Equal(t, len(d), n)
+
+	eq, err := storage.Equals(sp1, sp2, 1024)
+	assert.NoError(t, err)
+	assert.True(t, eq)
+
+	// Now modify sp2 a bit
+	for _, offset := range []int64{7, 8, 900, 3000} {
+		d = make([]byte, 6)
+		_, err = rand.Read(d)
+		assert.NoError(t, err)
+		n, err = sp2.WriteAt(d, offset)
+		assert.NoError(t, err)
+		assert.Equal(t, len(d), n)
+	}
+
+	eq, err = storage.Equals(sp1, sp2, 1024)
+	assert.NoError(t, err)
+	assert.False(t, eq)
+
+}
