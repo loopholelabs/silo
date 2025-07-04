efa4a48cc39fea63bd2c40b3034fb5b45c3c6b4e
Jimmy Moore <jamesmoore@loopholelabs.io>
2025-07-04 16:33:31 +0100

    Compression changes from jimmy/arch-388-silo-compression
    Signed-off-by: Jimmy Moore <jamesmoore@loopholelabs.io>

--- pkg/storage/protocol/packets/write_at_comp.go
+++ pkg/storage/protocol/packets/write_at_comp.go
@@ -1,116 +1,31 @@
 package packets
 
-import (
-	"bytes"
-	"encoding/binary"
-)
+var CompressionImpl = CompressRLE
 
-const rleMinLength = 4
+const CompressRLE = 0
+const CompressGzip = 1
+const CompressZeroes = 2
 
 func EncodeWriteAtComp(offset int64, data []byte) []byte {
-	var buff bytes.Buffer
-
-	buff.WriteByte(CommandWriteAt)
-	buff.WriteByte(WriteAtCompRLE)
-
-	b := make([]byte, 8)
-	binary.LittleEndian.PutUint64(b, uint64(offset))
-	buff.Write(b)
-
-	// Now RLE the data
-	areas := make(map[int]int, 0)
-
-	ld := byte(0)
-	length := 0
-	for i, d := range data {
-		if d != ld {
-			// Flush the current run
-			if length >= rleMinLength {
-				areas[i-length] = length
-			}
-			length = 1
-			ld = d
-		} else {
-			length++ // Run length...
-		}
+	switch CompressionImpl {
+	case CompressRLE:
+		return EncodeWriteAtCompRLE(offset, data)
+	case CompressZeroes:
+		return EncodeWriteAtCompZeroes(offset, data)
+	case CompressGzip:
+		return EncodeWriteAtCompGzip(offset, data)
 	}
-
-	if length > rleMinLength {
-		areas[len(data)-length] = length
-	}
-
-	// Now we encode the data...
-	p := 0
-	start := 0
-	for {
-		if p == len(data) {
-			break
-		}
-		// Check if it's a RLE range
-		l, ok := areas[p]
-		if ok {
-			// First flush any current range
-			if start != p {
-				dd := make([]byte, p-start+binary.MaxVarintLen32)
-				lpos := binary.PutUvarint(dd, uint64(p-start)<<1)
-				copy(dd[lpos:], data[start:p])
-				buff.Write(dd[:lpos+(p-start)])
-			}
-
-			// TODO: Encode the data
-			dd := make([]byte, binary.MaxVarintLen32+1)
-			lpos := binary.PutUvarint(dd, (uint64(l)<<1)|1)
-			dd[lpos] = data[p]
-			buff.Write(dd[:lpos+1])
-
-			p += l
-			start = p
-		} else {
-			p++
-		}
-	}
-
-	if start != p {
-		dd := make([]byte, p-start+binary.MaxVarintLen32)
-		lpos := binary.PutUvarint(dd, uint64(p-start)<<1)
-		copy(dd[lpos:], data[start:p])
-		buff.Write(dd[:lpos+(p-start)])
-	}
-
-	return buff.Bytes()
+	return EncodeWriteAtCompRLE(offset, data)
 }
 
 func DecodeWriteAtComp(buff []byte) (offset int64, data []byte, err error) {
-	if len(buff) < 10 || buff[0] != CommandWriteAt || buff[1] != WriteAtCompRLE {
-		return 0, nil, ErrInvalidPacket
+	switch CompressionImpl {
+	case CompressRLE:
+		return DecodeWriteAtCompRLE(buff)
+	case CompressZeroes:
+		return DecodeWriteAtCompZeroes(buff)
+	case CompressGzip:
+		return DecodeWriteAtCompGzip(buff)
 	}
-	off := int64(binary.LittleEndian.Uint64(buff[2:]))
-
-	var d bytes.Buffer
-
-	// Now read the RLE data and put it in d buffer...
-	p := 10
-	for {
-		if p == len(buff) {
-			break
-		}
-		// Read something
-		l, llen := binary.Uvarint(buff[p:])
-		p += llen
-		if (l & 1) == 0 {
-			// RAW
-			d.Write(buff[p : p+(int(l)>>1)])
-			p += (int(l) >> 1)
-		} else {
-			// RLE
-			val := buff[p]
-			// Write the value multiple times...
-			for i := 0; i < int(l>>1); i++ {
-				d.WriteByte(val)
-			}
-			p++
-		}
-	}
-
-	return off, d.Bytes(), nil
+	return DecodeWriteAtCompRLE(buff)
 }
